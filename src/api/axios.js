6e981793e8c9cc5a7595bc74ac08a29276cd6e4e
Angel Juarez <spotify27816@gmail.com>
2023-12-12 19:45:28 -0600

    Nueva ruta api ES2 3

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // dominio base al que simpre va a consultar
 const intance = axios.create({
-  baseURL: "https://35.168.86.243/api",
+  baseURL: "http://35.168.86.243/api",
   withCredentials: true,
 });
 
