c3f73392a27f64a75e5464c26530f9920cb80d57
Angel Juarez <spotify27816@gmail.com>
2023-12-13 23:42:58 -0600

    se cambio solicitudes axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 // configuraremos axios
 import axios from "axios";
-
+axios.defaults.withCredentials = true;
+axios.defaults.headers.crossDomain = true;
 // dominio base al que simpre va a consultar
 const intance = axios.create({
   baseURL: "https://setylsaback.guiatusemociones.com/api",
-  withCredentials: true,
 });
 
 export default intance;
