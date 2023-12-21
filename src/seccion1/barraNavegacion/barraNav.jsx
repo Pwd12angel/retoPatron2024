0f054dcb5b45492cab8caf5ce342cfc12a711599
Angel Juarez <spotify27816@gmail.com>
2023-12-21 16:31:51 -0600

    Se Modifico la ruta de salida

--- src/seccion1/barraNavegacion/barraNav.jsx
+++ src/seccion1/barraNavegacion/barraNav.jsx
@@ -27,7 +27,7 @@ const BarraNav = () => {
                     {isAutenticado ? (
                         <>
 
-                            <li> <Link to="/inicio" onClick={() => { cerrar(); }} >Salir <svg width="25px" fill="none" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <li> <Link to="/" onClick={() => { cerrar(); }} >Salir <svg width="25px" fill="none" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M11.996 3H3v18h9"></path>
                                 <path d="M16.5 16.5 21 12l-4.5-4.5"></path>
                                 <path d="M8 11.996h13"></path>
